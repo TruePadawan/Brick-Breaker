0ece388a6ca3c03f4811ca2cb261bd242951d54f
Hermes <hermeschigoziri@gmail.com>
2022-06-20 08:18:19 +0100

    Add wall collision detection and ball rebounding

---
+++ Engine/Ball.cpp
@@ -0,0 +1,67 @@
+#include "Ball.h"
+
+Ball::Ball(const Vector2D& pos, const Vector2D& ballVelocity)
+	:position(pos), velocity(ballVelocity)
+{
+	
+}
+
+bool Ball::isCollidingWith(const Rect& entity)
+{
+	bool colliding = false;
+	return false;
+}
+
+void Ball::move(float ft)
+{
+	position += velocity * ft;
+}
+
+bool Ball::isAtBoundary(const Rect& boundary)
+{
+	bool collidedWithWall = false;
+	Rect boundingRect = Rect::fromCenter(position, radius, radius);
+
+	if (boundingRect.left < boundary.left)
+	{
+		collidedWithWall = true;
+		reboundX();
+		position.x += boundary.left - boundingRect.left;
+	}
+	else if (boundingRect.right > boundary.right)
+	{
+		collidedWithWall = true;
+		reboundX();
+		position.x -= boundingRect.right - boundary.right;
+	}
+
+	if (boundingRect.top < boundary.top)
+	{
+		collidedWithWall = true;
+		reboundY();
+		position.y += boundary.top - boundingRect.top;
+	}
+	else if (boundingRect.bottom > Graphics::ScreenHeight)
+	{
+		collidedWithWall = true;
+		reboundY();
+		position.y -= boundingRect.bottom - boundary.bottom;
+	}
+
+	return collidedWithWall;
+}
+
+void Ball::draw(Graphics& gfx)
+{
+	SpriteCodex::DrawBall(position, gfx);
+}
+
+void Ball::reboundX()
+{
+	velocity.x = -velocity.x;
+}
+
+void Ball::reboundY()
+{
+	velocity.y = -velocity.y;
+}
---
+++ Engine/Ball.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "SpriteCodex.h"
+
+class Ball
+{
+public:
+	Ball(const Vector2D& pos, const Vector2D& ballVelocity);
+	bool isCollidingWith(const Rect& entity);
+	void move(float ft);
+	bool isAtBoundary(const Rect &boundary);
+	void draw(Graphics& gfx);
+private:
+	void reboundX();
+	void reboundY();
+private:
+	Vector2D position;
+	Vector2D velocity;
+	static constexpr float radius = 7.0f;
+};
+
