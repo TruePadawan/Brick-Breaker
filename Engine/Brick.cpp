b726e57563717468523bf5bc16b5be6823afe347
Hermes <hermeschigoziri@gmail.com>
2022-06-21 10:40:11 +0100

    handle ball and brick collision

--- Engine/Brick.cpp
+++ Engine/Brick.cpp
@@ -1,11 +1,51 @@
 #include "Brick.h"
 
 Brick::Brick(Rect& body, Color color)
-	:body(body), color(color), isDestroyed(false)
+	:body(body), color(color), destroyed(false)
 {
+}
+
+Rect Brick::getBoundingRect() const
+{
+	return body;
+}
+
+bool Brick::handleBallCollision(Ball& ball)
+{
+	bool didCollisionHappen = false;
+	Rect brickRect{ this->getBoundingRect() };
+	Rect ballRect{ ball.getBoundingRect() };
+
+	if (!destroyed && ballRect.isOverlapping(brickRect))
+	{
+		didCollisionHappen = true;
+		destroyed = true;
+
+		if (ballRect.left < brickRect.left)
+		{
+			ball.reboundX();
+		}
+		else if (ballRect.right > brickRect.right)
+		{
+			ball.reboundX();
+		}
+
+		if (ballRect.top < brickRect.top)
+		{
+			ball.reboundY();
+		}
+		else if (ballRect.bottom > Graphics::ScreenHeight)
+		{
+			ball.reboundY();
+		}
+	}
+	return didCollisionHappen;
 }
 
 void Brick::draw(Graphics& gfx)
 {
-	gfx.DrawRect(body, color);
+	if (!destroyed)
+	{
+		gfx.DrawRect(body, color);
+	}
 }
--- Engine/Brick.h
+++ Engine/Brick.h
@@ -1,15 +1,19 @@
 #pragma once
 #include "Rect.h"
+#include "Ball.h"
 #include "Graphics.h"
 
 class Brick
 {
 public:
+	Brick() = default;
 	Brick(Rect& body, Color color);
+	Rect getBoundingRect() const;
+	bool handleBallCollision(Ball& ball);
 	void draw(Graphics& gfx);
 private:
 	Rect body;
 	Color color;
-	bool isDestroyed;
+	bool destroyed;
 };
 
